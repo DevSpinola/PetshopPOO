dd2351ee06830afc2db805addc6face282d42a2b
Guilherme  Santos <guilherme.santos@quaestum.com.br>
2023-09-18 17:13:19 -0300

    Alterando Metodos e Resolvendo Bugs

--- Petshop/Modelos/Cadastro.cs
+++ Petshop/Modelos/Cadastro.cs
@@ -2,9 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Petshop
 {
@@ -13,7 +10,7 @@ namespace Petshop
         List<Animal> listaAnimais = new List<Animal>();
         List<Veterinário> listaVet = new List<Veterinário>();
         List<ConsultaMedica> listaConsultas = new List<ConsultaMedica>();
-
+        // Metodos Animais --------------------------------------------------------------------------------------------------------------------------------
         public void CadastrarAnimal()
         {
             //Método para cadastrar o pet -> animal
@@ -53,7 +50,7 @@ namespace Petshop
         {
             //Método para alterar dados no animal
             Console.WriteLine("---> Alteração de Cadastro do Animal <---");
-            Console.WriteLine("Informe o nome do animal para consultar dados: ");
+            Console.WriteLine("Informe o nome do animal para alterar dados: ");
             string animal = Console.ReadLine().ToUpper();
             if (listaAnimais.Any(a => a.nome.ToUpper() == animal))
             {
@@ -106,6 +103,9 @@ namespace Petshop
             {
                 Console.WriteLine($"Nome: {animal.nome}, Espécie: {animal.especie}");
             });
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
         }
         public void ConsultarAnimal()
         {
@@ -158,8 +158,11 @@ namespace Petshop
             {
                 Console.WriteLine($"Nenhum animal com o nome {animal} cadastrado.");
             }
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
         }
-
+        // Metodos Consulta --------------------------------------------------------------------------------------------------------------------------------
         public void AgendarConsulta()
         {
             Console.WriteLine("---->Agendamento Consulta<----");
@@ -187,7 +190,7 @@ namespace Petshop
             {
                 Console.WriteLine($"Não há nenhum animal com nome {animal} cadastrado");
             }
-                        
+
 
             Console.WriteLine("Digite alguma tecla para voltar ao Menu");
             Console.ReadKey();
@@ -238,7 +241,147 @@ namespace Petshop
             Console.WriteLine("Digite alguma tecla para voltar ao Menu");
             Console.ReadKey();
             Console.Clear();
+        }
+        // Métodos Vet --------------------------------------------------------------------------------------------------------------------------------
+        public void CadastrarVet()
+        {
+            Console.WriteLine("-->Cadastro de Veterinários<--");
+            Console.WriteLine("Nome do veterinário: ");
+            string nomeVet = Console.ReadLine();
+            Console.WriteLine("Especialidade: ");
+            string especialidade = Console.ReadLine();
+            Console.WriteLine("Número do CRMV:");
+            int CRMV = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dias da semana de trabalho: ");
+            string diasTrabalho = Console.ReadLine();
 
+            // Cria um objeto Veterinário com os dados informados pelo usuário.
+            Veterinário veterinario = new Veterinário
+            {
+                NomeVet = nomeVet,
+                Especialidade = especialidade,
+                CRMV = CRMV,
+                DiasTrabalho = diasTrabalho
+            };
+
+            // Adiciona o veterinário à lista de veterinários.
+            listaVet.Add(veterinario);
+
+            Console.WriteLine("Veterinário Cadastrado com Sucesso!");
+
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
+        }
+
+        public void ConsultarVet()
+        {
+            Console.WriteLine($"Nome do veterinário: ");
+            string nomeVetConsulta = Console.ReadLine();
+
+            // Verifica se um veterinário com o nome informado existe na lista.
+            if (listaVet.Any(a => a.NomeVet == nomeVetConsulta))
+            {
+                // Encontra o primeiro veterinário com o nome informado.
+                Veterinário veterinarioASerConsultado = listaVet.First(a => a.NomeVet == nomeVetConsulta);
+
+                // Exibe os detalhes do veterinário encontrado.
+                Console.WriteLine($"Nome:{veterinarioASerConsultado.NomeVet}");
+                Console.WriteLine($"Especialidade: {veterinarioASerConsultado.Especialidade}");
+                Console.WriteLine($"Número do CRMV: {veterinarioASerConsultado.CRMV}");
+                Console.WriteLine($"Dias da semana de trabalho: {veterinarioASerConsultado.DiasTrabalho}");
+            }
+            else
+            {
+                Console.WriteLine($"Veterinário {nomeVetConsulta} não existe!");
+            }
+
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
+        }
+
+        public void AlterarVet()
+        {
+            Console.WriteLine("--> Alteração de dados Veterinário <--");
+            try
+            {
+                Console.WriteLine($"Nome do veterinário a ser alterado: ");
+                string nomeVetConsulta = Console.ReadLine();
+
+                // Verifica se um veterinário com o nome informado existe na lista.
+                if (listaVet.Any(a => a.NomeVet == nomeVetConsulta))
+                {
+                    // Encontra o primeiro veterinário com o nome informado.
+                    Veterinário veterinarioEmAlteracao = listaVet.First(a => a.NomeVet == nomeVetConsulta);                    
+
+                    Console.WriteLine("Novo Nome: ");
+                    veterinarioEmAlteracao.NomeVet = Console.ReadLine();
+                    Console.WriteLine("Especialidade: ");
+                    veterinarioEmAlteracao.Especialidade = Console.ReadLine();
+                    Console.WriteLine("Número do CRMV:");
+                    veterinarioEmAlteracao.CRMV = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Dias da semana de trabalho: ");
+                    veterinarioEmAlteracao.DiasTrabalho = Console.ReadLine();
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Veterinário não existe!");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato de dados inválido");
+            }
+
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
+        }
+        public void ExcluirVet()
+        {
+            Console.WriteLine("--> Exclusão do Veterinário<--");
+            Console.WriteLine($"Nome do veterinário a ser excluído: ");
+            string nomeVetConsulta = Console.ReadLine();
+
+            // Verifica se um veterinário com o nome informado existe na lista.
+            if (listaVet.Any(a => a.NomeVet == nomeVetConsulta))
+            {
+                Console.WriteLine($"Digite {nomeVetConsulta} novamente para confirmar a exclusão");
+
+                // Lê o nome novamente para confirmar a exclusão.
+                if (nomeVetConsulta == Console.ReadLine())
+                {
+                    // Encontra o veterinário a ser excluído.
+                    Veterinário veterinarioASerExcluido = listaVet.First(a => a.NomeVet == nomeVetConsulta);
+
+                    // Remove o veterinário da lista e verifica se a remoção foi bem-sucedida.
+                    bool removeu = listaVet.Remove(veterinarioASerExcluido);
+                    Console.WriteLine(removeu ? "Veterinário removido com sucesso" : "O Veterinário não foi removido");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Veterinário não existe!");
+            }
+
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
+        }
+
+        public void ConsultarVetCadastrados()
+        {
+            // Percorre a lista de veterinários e exibe seus nomes e especialidades.
+            listaVet.ForEach(veterinario =>
+            {
+                Console.WriteLine($"Nome: {veterinario.NomeVet}, Especialidade: {veterinario.Especialidade}");
+            });
+
+            Console.WriteLine("Digite alguma tecla para voltar ao Menu");
+            Console.ReadKey();
+            Console.Clear();// Limpa o Console
         }
 
     }
--- Petshop/Program.cs
+++ Petshop/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace Petshop
 {
@@ -12,18 +9,51 @@ namespace Petshop
         static void Main(string[] args)
         {
             //Criando objeto da classe cadastro
-            Cadastro cadastro = new Cadastro();           
+            Cadastro cadastro = new Cadastro();
 
             void MenuPrograma()
             {
+                Console.Clear();
                 Console.WriteLine("---> Bem vindos ao PetShop da Villa <---");
+                Console.WriteLine("Escolha a opção desejada:");
+                Console.WriteLine("1 - Acessar aba Animais");
+                Console.WriteLine("2 - Acessar aba Veterinario");
+                Console.WriteLine("3 - Acessar aba Consulta Medica");
+                Console.WriteLine("4 - Sair do programa");
+                int.TryParse(Console.ReadLine(), out int opcao);
+                switch (opcao)
+                {
+                    case 1:
+                        MenuAnimal();
+                        break;
+
+                    case 2:
+                        MenuVeterinario();
+                        break;
+
+                    case 3:
+                        MenuConsulta();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Obrigado por utilizar. Até logo!");
+                        Thread.Sleep(2000);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        MenuPrograma();
+                        break;
+                }
+            }
+            void MenuAnimal()
+            {
                 Console.WriteLine("Escolha a opção desejada:");
                 Console.WriteLine("1 - Cadastrar animal");
                 Console.WriteLine("2 - Consultar animal");
                 Console.WriteLine("3 - Alterar animal");
                 Console.WriteLine("4 - Excluir animal");
                 Console.WriteLine("5 - Consultar todos os animais cadastrados");
-                Console.WriteLine("6 - Sair do programa");
+                Console.WriteLine("6 - Voltar ao Menu Principal");
                 int.TryParse(Console.ReadLine(), out int opcao);
                 switch (opcao)
                 {
@@ -50,7 +80,7 @@ namespace Petshop
                         cadastro.ConsultarAnimaisCadastrados();
                         break;
                     case 6:
-                        Console.WriteLine("Obrigado por utilizar. Até logo!");                        
+                        MenuPrograma();
                         break;
 
                     default:
@@ -58,9 +88,90 @@ namespace Petshop
                         MenuPrograma();
                         break;
                 }
+                MenuPrograma();
+
+            }
+            void MenuVeterinario()
+            {
+                Console.WriteLine("Escolha a opção desejada:");
+                Console.WriteLine("1 - Cadastrar Veterinario");
+                Console.WriteLine("2 - Consultar Veterinario");
+                Console.WriteLine("3 - Alterar Veterinario");
+                Console.WriteLine("4 - Excluir Veterinario");
+                Console.WriteLine("5 - Consultar todos os Veterinarios cadastrados");
+                Console.WriteLine("6 - Voltar ao Menu Principal");
+                int.TryParse(Console.ReadLine(), out int opcao);
+                switch (opcao)
+                {
+                    case 1:
+                        cadastro.CadastrarVet();
+                        MenuPrograma();
+                        break;
+
+                    case 2:
+                        cadastro.ConsultarVet();
+                        MenuPrograma();
+                        break;
+
+                    case 3:
+                        cadastro.AlterarVet();
+                        MenuPrograma();
+                        break;
+
+                    case 4:
+                        cadastro.ExcluirVet();
+                        MenuPrograma();
+                        break;
+                    case 5:
+                        cadastro.ConsultarVetCadastrados();
+                        MenuPrograma();
+                        break;
+                    case 6:
+                        MenuPrograma();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        MenuPrograma();
+                        break;
+                }
+
+            }
+            void MenuConsulta()
+            {
+                Console.WriteLine("Escolha a opção desejada:");
+                Console.WriteLine("1 - Agendar Consulta");
+                Console.WriteLine("2 - Registrar Consulta");
+                Console.WriteLine("3 - Exibir Consulta");
+                Console.WriteLine("4 - Voltar ao Menu Principal");
+                int.TryParse(Console.ReadLine(), out int opcao);
+                switch (opcao)
+                {
+                    case 1:
+                        cadastro.AgendarConsulta();
+                        MenuPrograma();
+                        break;
+                    case 2:
+                        cadastro.RegistrarConsulta();
+                        MenuPrograma();
+                        break;
+                    case 3:
+                        cadastro.ExibirConsulta();
+                        MenuPrograma();
+                        break;
+                    case 4:
+                        MenuPrograma();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        MenuPrograma();
+                        break;
+
+                }
             }
             MenuPrograma();
-
         }
+
+
     }
 }
